094ea01d86c8914f52d1b511c4a64021edc6a9fd
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-27 18:27:35 +0530

    ADDED: Welcome Message

---
+++ com/generics/MaximumOfThreeTest.java
@@ -0,0 +1,10 @@
+package com.generics;
+
+public class MaximumOfThreeTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Maximum Of Three Generic Program");
+	}
+
+}
